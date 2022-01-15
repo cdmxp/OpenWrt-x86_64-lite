c061a302c7ca8bb46295562af5a839938b7f3f3b
cdmxp <58542703+cdmxp@users.noreply.github.com>
2022-01-15 19:39:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.88.2/g' package/base-files/files/bin/config_generate
-# Modify Default string Default string
-sed -i 's/h=${g}' - '${a}${b}${c}${d}${e}${f}/h=${a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
